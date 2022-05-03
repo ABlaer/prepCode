0e8af75046378447f10824fee73870dfecb92482
almogblaer <blaer@post.bgu.ac.il>
2022-05-03 12:39:36 +0300

    writing the setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='prepCode',
+    version='1.1',
+    install_requires=['numpy', 'obspy', 'pandas', 'glob', 'matplotlib'],
+    url='www.github.com/ABlaer',
+    license='MIT',
+    author='almogblaer',
+    author_email='blaer@post.bgu.ac.il',
+    description='Preparation code for synthetic seismic data, that mimics real ones',
+    python_requires='>3'
+)
+
